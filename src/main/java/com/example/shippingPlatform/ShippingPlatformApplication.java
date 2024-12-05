ebc08d4ce954b2c3943ae4d85af7feca78b73565
pasr79 <psar1987@wp.pl>
2024-12-05 16:25:28 +0100

    Initial commit

---
+++ src/main/java/com/example/shippingPlatform/ShippingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.shippingPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShippingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShippingPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/shippingPlatform/ShippingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shippingPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShippingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
